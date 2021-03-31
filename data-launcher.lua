3bf5a3358341dd21af65bbe21331457df30bb5da
TeamPopplio <teampopplio@protonmail.com>
2021-03-31 15:06:17 -0700

    fix first launch

--- data-launcher.lua
+++ data-launcher.lua
@@ -29,7 +29,7 @@ local data = {
 }
 local info = love.filesystem.getInfo("settings.txt")
 if not info then --does not exist
-    love.filesystem.write("settings.txt", "{}")
+    love.filesystem.write("settings.txt", "{Version={Major=0,Minor=0,Patch=0}}")
 end
 local unpack = TSerial.unpack(love.filesystem.read("settings.txt"), true)
 for k,v in pairs(unpack) do data[k] = v end --combine data and unpack tables
